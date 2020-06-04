7ac0d026a3d6ded76b930d5c9a95a9502f5a4a72
Evadne Meraki <evadne.meraki@gmail.com>
2020-06-03 23:53:06 -0300

    Added the last two calculations required

--- 3. Do the Math/Do the Math.c
+++ 3. Do the Math/Do the Math.c
@@ -6,13 +6,16 @@ int main () {
 
     puts("Please, enter an integer");
     scanf ("%i", &firstNum);
-    puts("Please, enter one more integer to begin the calculations");
+    puts("Please, enter one more integer to begin the calculations\n");
     scanf ("%i", &secondNum);
+    sum = firstNum+secondNum; 
 
-    printf("The sum of %i and %i is equal to %i \n", firstNum, secondNum, firstNum+secondNum);
+    printf("The sum of %i and %i is equal to %i \n", firstNum, secondNum, sum);
     printf("The difference between %i and %i is equal to %i \n", firstNum, secondNum, firstNum-secondNum);
     printf("The product of %i and %i is equal to %i \n", firstNum, secondNum, firstNum*secondNum);
-    printf("The quotient of %i divided %i is equal to %.2f \n\n", firstNum, secondNum, (float)firstNum/secondNum);
+    printf("The quotient of %i divided %i is equal to %.2f \n", firstNum, secondNum, (float)firstNum/secondNum);
+    printf("The mean of %i and %i is equal to %.2f \n", firstNum, secondNum, sum/2);
+    printf("The double of the difference between %i and the half of %i is equal to %.2f \n\n", firstNum, secondNum, (float)2*(firstNum-(secondNum/2)));
 
     system ("pause");
     return 0;
