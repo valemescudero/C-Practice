ca6b0f316a0767a85ddf02f420a0ac5a9180e4fb
Evadne Meraki <evadne.meraki@gmail.com>
2020-06-06 17:11:53 -0300

    New! Hour you doing?

--- 09. Wrecktangle/Wrecktangle.c
+++ 09. Wrecktangle/Wrecktangle.c
@@ -1,11 +1,12 @@
-/* Build a program that asks for the sides of a rectangle and outputs its area and perimeter */
+/* Build a program that asks for the sides of a rectangle and outputs
+its area and perimeter */
 
 #include <stdio.h>
 #include <math.h> 
   
 int main () { 
     float base = 0, height = 0, perimeter = 0, area = 0;
-    printf("Welcome to Wrecktangle.\n\n");
+    printf("\nWelcome to Wrecktangle.\n\n");
     printf("    ----------\n   |          |\n h |          |\n   |          |\n    ----------\n      b\n\n");    
     printf("To calculate the perimeter and area of your rectangle,\nplease enter the length of the base (b) without unit symbols.\n");
 
@@ -27,7 +28,7 @@ int main () {
 
     perimeter = (base*2)+(height*2);
     area = base*height;
-    printf("\nThe perimeter of your rectangle is %.2f units.\n The perimeter of a geometric shape is obtained by adding the lengths of all its sides.\n In a rectangle this equals the sum of the doubles for both the height and base.\n\n", perimeter);
+    printf("\nThe perimeter of your rectangle is %.2f units.\n The perimeter of a geometric shape is obtained by adding the lengths of all its sides.\n In a rectangle this equals the sum of the doubles of both the height and base.\n\n", perimeter);
     printf("\nThe area of your rectangle is %.2f units square.\nThe area of a rectangle is calculated by multiplying its height by its base.\n\n",  area);
 
     system ("pause");
---
+++ 11. Hour you doing/Hour you doing.c
@@ -0,0 +1,54 @@
+/* Construye un programa que permita ingresar 2 tiempos, expresados en horas, minutos y
+segundos, el mismo debe emitir por pantalla la suma de ambos (también en horas, minutos
+y segundos). */
+
+#include <stdio.h>
+#include <math.h> 
+  
+int main () { 
+    printf(" WELCOME TO Hour you doing?\n");
+    printf(" Given a starting time and a lapse of time, we'll able to tell you the ending time for your task.\n\n");
+    
+    int startingTime[3], lapse[3], endingTime[3], days=0;
+
+    do {
+        printf(" Please, enter your starting time (24-hour clock/military time),\n including minutes and seconds separated with spaces.\n");
+        scanf("%d %d %d", &startingTime[0], &startingTime[1], &startingTime[2]);
+        if (startingTime[1]>59 || startingTime[2]>59 || startingTime[1]<0 || startingTime[2]<0 || startingTime[0]<0 || startingTime[0]>=24) {
+            printf("\n Sorry, one or more of the entered values is invalid.\n");
+        };
+    } while (startingTime[1]>59 || startingTime[2]>59 || startingTime[1]<0 || startingTime[2]<0 || startingTime[0]<0 || startingTime[0]>=24);
+
+     do {
+       printf("\n Please, enter the lapse of time you'd like to add,\n including minutes and seconds separated with spaces.\n");
+        scanf("%d %d %d", &lapse[0], &lapse[1], &lapse[2]);
+        if (lapse[1]>59 || lapse[2]>59 || lapse[1]<0 || lapse[2]<0 || lapse[0]<0) {
+            printf("\n Sorry, one or more of the entered values is invalid.\n");
+        };        
+     } while (lapse[1]>59 || lapse[2]>59 || lapse[1]<0 || lapse[2]<0 || lapse[0]<0);
+   
+    endingTime[2] = startingTime[2] + lapse[2];
+    endingTime[1] = startingTime[1] + lapse[1];
+    endingTime[0] = startingTime[0] + lapse[0];
+
+    while (endingTime[2]>59) {
+        endingTime[1]++;
+        endingTime[2] -= 60;
+    };
+    while (endingTime[1]>59) {
+        endingTime[0]++;
+        endingTime[1] -= 60;
+    };
+    while (endingTime[0]>=24) {
+        days++;
+        endingTime[0] -= 24;
+    };
+    
+    if (days>0) {
+        printf("\n The ending time is going to be at:\n %d hours %d minutes %d seconds (24-hour clock/military time) in %d day(s).\n\n", endingTime[0], endingTime[1], endingTime[2], days);
+    } else {
+        printf("\n The ending time is going to be at:\n %d hours %d minutes %d seconds (24-hour clock/military time).\n\n", endingTime[0], endingTime[1], endingTime[2]);
+    };
+    system ("pause");
+    return 0;
+};
