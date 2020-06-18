e06500000fb7dca9eaf77686cd60b67c255989c9
Evadne Meraki <evadne.meraki@gmail.com>
2020-06-18 01:21:28 -0300

    New Folder! Second Module. New! Is it Odd and Are you Positive

---
+++ Second Module/1. Is it Odd/1. Is it Odd.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <math.h>
+
+void evenF(double n);
+
+int main(){
+    double n = 0; // given number
+    int option = 0;
+
+    printf("\n Welcome. This program can determine whether a given number is EVEN or ODD.\n\n");
+    printf(" What would you like to do?\n\n 1 Enter a number\n 2 Exit\n\n ");
+    scanf("%d", &option);
+
+    do {
+        switch (option) {
+            case 1:
+                printf("\n Please, enter a number:\n ");
+                scanf ("%lf", &n);
+                evenF(n);
+                printf("\n\n Would you like to test another number?\n\n 1 Yes\n 2 No (Exit)\n\n ");
+                scanf("%d", &option);
+                break;
+            case 2:
+                break;
+            default:
+                printf("\n Sorry, invalid option.");
+                printf("\n\n Would you like to test a number?\n\n 1 Yes\n 2 No (Exit)\n\n ");
+                scanf("%d", &option);
+                break;
+        }
+    } while (option != 2);
+    
+    return 0;
+};
+
+void evenF(double n) {
+
+    if (fmod(n,2) == 0) {
+        printf("\n The given number %f is EVEN", n);
+    } else {
+        printf("\n The given number %f is ODD", n);
+    };
+
+};
+
+
---
+++ Second Module/2. Are you Positive/2. Are you positive.c
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include <math.h>
+
+void signF(double n);
+
+int main(){
+    double n = 0; // given number
+    int option = 0;
+
+    printf("\n Welcome. This program can determine whether a given number is POSITIVE, NEGATIVE or ZERO.\n\n");
+    printf(" What would you like to do?\n\n 1 Enter a number\n 2 Exit\n\n ");
+    scanf("%d", &option);
+
+    do {
+        switch (option) {
+            case 1:
+                printf("\n Please, enter a number:\n ");
+                scanf ("%lf", &n);
+                signF(n);
+                printf("\n\n Would you like to test another number?\n\n 1 Yes\n 2 No (Exit)\n\n ");
+                scanf("%d", &option);
+                break;
+            case 2:
+                break;
+            default:
+                printf(" Sorry, invalid option.");
+                printf("\n\n Would you like to test a number?\n\n 1 Yes\n 2 No (Exit)\n\n ");
+                scanf("%d", &option);
+                break;
+        }
+    } while (option != 2);
+    
+    return 0;
+};
+
+void signF(double n) {
+
+    if (n == 0) {
+        printf("\n The given number %f equals to ZERO.", n);
+    } else if (n > 0) {
+        printf("\n The given number %f is POSITIVE.", n);
+    } else {
+        printf("\n The given number %f is NEGATIVE.", n);
+    };
+
+};
+
+
