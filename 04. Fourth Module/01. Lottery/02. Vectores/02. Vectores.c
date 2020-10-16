2af535e21fea7864154bd52e5c78ed8e7b974e32
Evadne Meraki <valemescudero@gmail.com>
2020-10-15 21:22:02 -0300

    New! Vectores

---
+++ 04. Fourth Module/01. Lottery/02. Vectores/02. Vectores.c
@@ -0,0 +1,14 @@
+/* Escribe un programa con un menú que defina las siguientes funciones y emita los resultados, la
+dimensión del vector será de 10 (elementos):
+a) Cargar m elementos de un vector por teclado. Deberá considerar que no podrá superar el
+máximo de elementos de vector: void cargar(int vect[], int unNum);
+b) Leer elementos de un vector hasta encontrar el número entero a. Deberá retornar el
+número de elementos que ha leído sin contar el entero a. int leerMarca(int
+vect[]);
+c) Dado un elemento y dado un vector de enteros, desarrolle una función que devuelva el
+número de apariciones del elemento en el vector. int veces(int valor, int
+vect[], int unNum);
+d) Invertir los elementos del vector sin utilizar otro vector. Por ejemplo, el vector formado por
+los enteros: 1 2 3, debe quedar 3 2 1. void invertirOrden(int vect[], int
+unNum);
+ */
